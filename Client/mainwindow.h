5387c397d9e1432f979de0bb736d16bc5cd353e7
Гусев Иван <Ivan-Ivanich-777>
2026-04-09 10:38:00 +0300

    Клиент: mainwindow

---
+++ Client/mainwindow.h
@@ -0,0 +1,25 @@
+﻿/**
+ * @file mainwindow.h
+ * @brief Главное окно приложения
+ * @author Гусев Иван
+ */
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+#include <QStackedWidget>
+class AuthForm;
+class TitleForm;
+class TaskForm;
+class DashboardForm;
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *p = nullptr);
+private:
+    QStackedWidget *m_stack;
+    TitleForm *m_title;
+    TaskForm *m_task;
+    DashboardForm *m_dash;
+    AuthForm *m_auth;
+};
+#endif
